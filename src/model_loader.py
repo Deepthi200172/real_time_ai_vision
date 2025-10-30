0396e298ba3997a1631980ecbc68d8dd515601b3
Deepthi Rajagopal Gajendra <deepthir.g.271@gmail.com>
2025-10-30 13:54:45 +0100

    Update model_loader.py
    tensorflow package error resolved 
    
    Signed-off-by: Deepthi Rajagopal Gajendra  <deepthir.g.271@gmail.com>

--- src/model_loader.py
+++ src/model_loader.py
@@ -1,4 +1,3 @@
-from keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
 from keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input, decode_predictions
 def load_model():
     """Load pretrained MobileNetV2 model"""
